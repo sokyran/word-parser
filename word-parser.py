922b6558f589342c9b4b7c0fe349e4132a6c1345
sokyran <karass0001@gmail.com>
2020-04-21 15:16:31 +0300

    Redacted script. Added option to enter folder location from cmd.

--- word-parser.py
+++ word-parser.py
@@ -6,36 +6,23 @@ from pathlib import Path
 word = win32com.client.Dispatch("Word.Application")
 word.visible = False
 
-# folder = input('Enter path to folder with doc files: ')
-# file_name = folder.split('\\')[-2]
+def collect_data(folder, write_file):
+	for root, dirs, files in os.walk(folder):
+		for i in files:
+			if str(i).endswith('.doc') | str(i).endswith('.docx'):
+				wb = word.Documents.Open(os.path.join(root, i))
+				doc = word.ActiveDocument
+				write_file.write('\n\n' + str(i).upper() + '\n\n')
+				write_file.write(doc.Range().Text)
+				print('\nDone')
+				doc.Close()
 
-# Previous folder settings
-# folder + '\\{}.txt'.format(file_name)
+def main():
+	#'C:\\Users\\yurii\\Desktop\\Me\\Course 2\\Пархом\\Лабораторні роботи ОС\\admin_redak\\'
+	folder = input("Enter folder location:\n") + "\\"
+	file_name = folder.split('\\')[-2]
+	with open(folder + f"\\{file_name}_collected.txt", 'w+', encoding='utf-8') as f:
+		collect_data(folder, f)
+	word.Quit()
 
-
-def handle_folder(folder, f):
-	entries = Path(folder)
-	for i in entries.iterdir():
-		if i.is_dir():
-			handle_folder(i, f)
-			print(f)
-			print(i, ' is folder')
-		elif str(i).endswith('.doc') | str(i).endswith('.docx'):
-			handle_doc(i, f)
-
-
-def handle_doc(docum, file):
-	print(docum)
-	wb = word.Documents.Open(str(docum))
-	doc = word.ActiveDocument
-	file.write('\n\n' + str(docum).upper() + '\n\n')
-	file.write(doc.Range().Text)
-	print('\nDone')
-	doc.Close()
-
-
-folder = 'C:\\Users\\yurii\\Desktop\\Me\\Course 2\\Базы данных\\Материалы\\'
-with open(folder + '\\all_lections.txt', 'wb+', encoding='utf-8') as f:
-	handle_folder(folder, f)
-
-word.Quit()
+main()
